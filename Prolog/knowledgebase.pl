320b4eb1a65e9819e915b65bc8f5490a056d7057
Micha de Groot <git@michadegroot.nl>
2016-09-09 12:32:24 +0200

    sme more symptoms in the knowledgebase

--- Prolog/knowledgebase.pl
+++ Prolog/knowledgebase.pl
@@ -1,10 +1,18 @@
+% operators die we moeten toevoegen
 op(250, xfy, of).
 op(300, xfy, en).
 op(800, fx, als).
 op(700, xfy, dan).
 op(100, fy, niet).
 
+% symptomen van mazelen. Bron: wikipedia
 koorts als mazelen.
 hoesten als mazelen.
 loopneus als mazelen.
+
+% symptomen van roodvonk.
+koorts als roodvonk.
+hoofdpijn als roodvonk.
+braken als roodvonk.
+aardbeientong of frambozentong als roodvonk.
 
