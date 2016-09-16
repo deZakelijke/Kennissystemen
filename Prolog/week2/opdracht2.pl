ae480abb1d3c8e4ee315fd211a0cb02905955f25
Micha de Groot <git@michadegroot.nl>
2016-09-16 16:46:28 +0200

    Almost finished

--- Prolog/week2/opdracht2.pl
+++ Prolog/week2/opdracht2.pl
@@ -1,6 +1,7 @@
 % Opdracht 2 Micha de Groot en Kaj Meijer
 
 
+% Alle teksten die gebruikt worden
 openingtext(X):-
     X = 'Welkom bij onze diagnoseprogramma. We gaan u zo een aantal vragen stellen over uw symptomen. Beantwoord de vragen door het antwoord te typen in kleine letters met een punt er achter alstublieft.\n'.
 
@@ -18,7 +19,27 @@ herhaalVraag(X):-
 incorrectAntwoord(X):-
     X = 'Dat is geen valide antwoord. Graag antwoorden met ja of nee'.
 
+afsluiting(X):-
+    X = '\nDankuwel voor het gebruiken van onze diagnosesoftware.'.
+
+geenUitsluitsel(X):-
+    X = 'Het is ons niet gelukt om te betpalen welke ziekte u heeft'.
+
 maakData([[mazelen,[hoofdpijn,buikpijn]],[rode_hond,[hoofdpijn,misselijk]]]).
+
+maakSymptomen([[_,Symptomen]],Symptomen).
+maakSymptomen([[_,Symptomen]|Ziektes],GoedeSymptomen):-
+    maakSymptomen(Ziektes,Symptomen2),
+    append(Symptomen,Symptomen2,MeerSymptomen),
+    set(MeerSymptomen,GoedeSymptomen).
+    
+set([],[]).
+set([H|T],[H|Out]) :-
+    not(member(H,T)),
+    set(T,Out).
+set([H|T],Out) :-
+    member(H,T),
+    set(T,Out).
 
 % Checks to find if Symptoom is a symptom of ziekte
 matchZiekte([Ziekte, Symptomen],Symptoom,[Ziekte,SymptomenNew]):-
@@ -42,45 +63,37 @@ zoekZiekte([Ziekte|Ziektes],Symptoom,Out):-
     Out = ZiektesOut).
 
 % Pakt het volgende symptoom uit de database waar nog niet naar is gevraagd
-volgendSymptoom([[Ziekte|[Symptomen]]|Ziektes],AlGevraagd,Symptoom):-
-    (member(Symptoom,Symptomen),
-    not(member(Symptoom,AlGevraagd)));
-    volgendSymptoom(Ziektes,Algevraagd,Symptoom).
-
-% Deze versie is voor als de eerste ziekte in de lijst geen symptomen meer heeft waar niet naar gevraagd is.
-volgendSymptoom([[_|[Symptomen]]|Ziektes],AlGevraagd,Symptoom):-
-   permutation(Symptomen,AlGevraagd),
-   volgendSymptoom(Ziektes,AlGevraagd,Symptoom).
+volgendSymptoom(MogelijkeSymptomen,AlGevraagd,Symptoom):-
+    member(Symptoom,MogelijkeSymptomen),
+    not(member(Symptoom,AlGevraagd)).
 
 % Vraag naar een volgend symptoom, herhaal tot er een gevonden is
-vraagSymptoom(Data,AlGevraagd,SymptoomOut,AlGevraagdNew):-
+vraagSymptoom(AlGevraagd,SymptoomOut,AlGevraagdNew,MogelijkeSymptomen):-
     volgendeVraag(Vraag), 
     write(Vraag),
-    volgendSymptoom(Data,AlGevraagd,SymptoomIn),
-    write(SymptoomIn),
-    write('\n'),
+    volgendSymptoom(MogelijkeSymptomen,AlGevraagd,SymptoomIn),
+    write(SymptoomIn),nl,
     antwoord(Antwoord),
     (Antwoord = ja,
     SymptoomOut = SymptoomIn,
     AlGevraagdNew = AlGevraagd);
     (Antwoord = nee,
     AlGevraagdUpdate = [SymptoomIn|Algevraagd],
-    vraagOpnieuw(Data,AlGevraagdUpdate,SymptoomOut,AlGevraagdNew)).
+    vraagOpnieuw(AlGevraagdUpdate,SymptoomOut,AlGevraagdNew,MogelijkeSymptomen)).
 
 % afhandelen wat er moet gebeuren als iemand nee antwoord op een vraag
-vraagOpnieuw(Data,AlGevraagd,SymptoomOut,AlGevraagdNew):-
+vraagOpnieuw(AlGevraagd,SymptoomOut,AlGevraagdNew,MogelijkeSymptomen):-
     herhaalVraag(Vraag),
     write(Vraag),
-    volgendSymptoom(Data,AlGevraagd,SymptoomIn),
-    write(SymptoomIn),
-    write('\n'),
+    volgendSymptoom(MogelijkeSymptomen,AlGevraagd,SymptoomIn),
+    write(SymptoomIn),nl,
     antwoord(Antwoord),
-    (Antwoord = ja,
+    ((Antwoord = ja,
     SymptoomOut = SymptoomIn,
     AlGevraagdNew = AlGevraagd);
     (Antwoord = nee,
     AlGevraagdUpdate = [SymptoomIn|AlGevraagd],
-    vraagOpnieuw(Data,AlGevraagdUpdate,SymptoomOut,AlGevraagdNew)).
+    vraagOpnieuw(AlGevraagdUpdate,SymptoomOut,AlGevraagdNew,MogelijkeSymptomen))).
 
 % ask for a ja or a nee, otherwise retry
 antwoord(CorrectAntwoord):-
@@ -90,31 +103,35 @@ antwoord(CorrectAntwoord):-
     (Antwoord = nee,
     CorrectAntwoord = nee),!;
     (incorrectAntwoord(X),
-    write(X),
+    write(X),nl,
     antwoord(CorrectAntwoord))).
 
 % Diagnosticeer tot er nog één ziekte is
-diagnosticeer([LaatsteZiekte],LaatsteZiekte,_).
+diagnosticeer([LaatsteZiekte],LaatsteZiekte,_,_).
 
-diagnosticeer(Data,GereduceerdeData,AlGevraagd):-
-    vraagSymptoom(Data,AlGevraagd,Symptoom,AlGevraagdNew),
+diagnosticeer(Data,GereduceerdeData,AlGevraagd,MogelijkeSymptomen):-
+    vraagSymptoom(AlGevraagd,Symptoom,AlGevraagdNew,MogelijkeSymptomen),
     zoekZiekte(Data,Symptoom,NewData),
-    diagnosticeer(NewData,GereduceerdeData,AlGevraagdNew).
+    delete(MogelijkeSymptomen,Symptoom,RedSymptomen),
+    diagnosticeer(NewData,GereduceerdeData,AlGevraagdNew,RedSymptomen).
 
 
 diagnose:-
     maakData(Data),
+    maakSymptomen(Data,MogelijkeSymptomen),
     openingtext(T1),
     write(T1),
+    write(MogelijkeSymptomen),
     eersteVraag(T2),
     write(T2),
     read(Symp1),
     zoekZiekte(Data,Symp1,NewData),
-    diagnosticeer(NewData,[Ziekte|_],[]),!,
+    delete(MogelijkeSymptomen,Symp1,RedSymptomen),
+    diagnosticeer(NewData,[Ziekte|_],[],RedSymptomen),!,
     write('Je hebt: '),
-    write(Ziekte).
-    %ask(NewData,Symp2),
-    %zoekZiekte(NewData,Symp1,NewData2),
-    %write(NewData2).
+    write(Ziekte),
+    afsluiting(Afsluiting),
+    write(Afsluiting).
+    
 
 
