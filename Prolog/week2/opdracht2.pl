89f7ac84814acf96f4b2f3498ee602a29a431477
Micha de Groot <git@michadegroot.nl>
2016-09-16 17:12:52 +0200

    shoved some things together

--- Prolog/week2/opdracht2.pl
+++ Prolog/week2/opdracht2.pl
@@ -1,14 +1,26 @@
 % Opdracht 2 Micha de Groot en Kaj Meijer
 
+% Knowledge base van Micha de Groot-10434410 en Kaj Meijer-10509534
+% studie@michadegroot.nl en k.d.meijer17@gmail.com
+% Kennissystemen 2016 eerste opdracht
+
+% operators die we moeten toevoegen
+:- op(300, xfy, en).
+:- op(700, xfy, verklaart).
+
+en(A,B):-
+    A,
+    B.
+
+verklaart(A,B):- A,B.
+verklaart(A,_):- niet(A).
 
 % Alle teksten die gebruikt worden
 openingtext(X):-
     X = 'Welkom bij onze diagnoseprogramma. We gaan u zo een aantal vragen stellen over uw symptomen. Beantwoord de vragen door het antwoord te typen in kleine letters met een punt er achter alstublieft.\n'.
 
 eersteVraag(X):-
-    X = 'De eerste vraag is: Wat is één van uw symptomen? U kunt kiezen uit de volgende opties:
-    hoofdpijn
-    buikpijn\n'.
+    X = 'De eerste vraag is: Wat is één van uw symptomen? U kunt kiezen uit de volgende opties:'.
 
 volgendeVraag(X):-
     X = 'Dankuwel. Heeft u last van het volgende symptoom? graag met ja of nee beantwoorden.\n'.
@@ -25,7 +37,49 @@ afsluiting(X):-
 geenUitsluitsel(X):-
     X = 'Het is ons niet gelukt om te betpalen welke ziekte u heeft'.
 
-maakData([[mazelen,[hoofdpijn,buikpijn]],[rode_hond,[hoofdpijn,misselijk]]]).
+maakData(ZiekteSymptomenLijst):- % AANGEPAST
+	open('knowledgebase2.txt', read, Str),
+	read_kb(Str, KB),!,
+	close(Str),
+	mzl(KB, ZSL, ASL),
+	delete(ZSL, end_of_file, ZiekteSymptomenLijst). % WRITES WEG
+
+
+read_kb(Stream, []):-
+	at_end_of_stream(Stream).
+
+read_kb(Stream, [H|T]):-
+	\+ at_end_of_stream(Stream),
+	read(Stream, H),
+	read_kb(Stream, T).
+
+mzl(Input, Output, ASL):-
+	mzl(Input, [], Output, ASL).
+
+mzl([H|T], Lijst, EindLijst, ASL):-
+	mzsc(H, ZiekteSymptoomCombinatie, SymptomenLijst),
+	append(Lijst, [ZiekteSymptoomCombinatie], ZiekteLijst),
+	append(ASL, ASL1, ASL2),
+	mzl(T, ZiekteLijst, EindLijst, ASL2).
+
+mzl(H, Lijst, EindLijst, _):-
+append(Lijst, H, EindLijst).
+
+mzsc(Ziekte verklaart Symptomen, ZiekteSymptoomCombinatie, SymptomenLijst):-
+	msl(Symptomen, SymptomenLijst),
+	ZiekteSymptoomCombinatie = [Ziekte, SymptomenLijst].
+
+
+msl(Symptomen, SymptomenLijst):-
+	msl(Symptomen, [], SymptomenLijst).
+
+msl(Symptoom en Symptomen, Lijst, EindLijst):-
+	append([Symptoom], Lijst, NieuweLijst),
+	msl(Symptomen, NieuweLijst, EindLijst). 	
+
+msl(Symptoom, SymptomenLijst, EindLijst):-
+	not(Symptoom = _ en _),
+	append([Symptoom], SymptomenLijst, EindLijst).
 
 maakSymptomen([[_,Symptomen]],Symptomen).
 maakSymptomen([[_,Symptomen]|Ziektes],GoedeSymptomen):-
@@ -121,9 +175,9 @@ diagnose:-
     maakSymptomen(Data,MogelijkeSymptomen),
     openingtext(T1),
     write(T1),
-    write(MogelijkeSymptomen),
     eersteVraag(T2),
-    write(T2),
+    write(T2),nl,
+    write(MogelijkeSymptomen),nl,
     read(Symp1),
     zoekZiekte(Data,Symp1,NewData),
     delete(MogelijkeSymptomen,Symp1,RedSymptomen),
