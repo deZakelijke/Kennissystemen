7bc7f1d5dc5dba037563465443f45fd97e9251e7
deZakelijke <git@michadegroot.nl>
2016-09-20 18:29:21 +0200

    Started on week3

---
+++ Prolog/week3/diagnoseProgram.pl
@@ -0,0 +1,14 @@
+% Made by Micha de Groot and Kaj Meijer
+
+box(1,a1).
+box(1,a1).
+box(2,m1).
+box(3,m1).
+box(a1,t1).
+box(m1,t2).
+box(m2,t3).
+box(t1,m2).
+box(t2,a2).
+box(t3,a2).
+box(a2,6).
+
