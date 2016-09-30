d0bbb37d6f976c0ec77066f7fc5d9abf2a7eb8ca
Kajzzer <k.d.meijer17@gmail.com>
2016-09-30 10:56:48 +0200

    Add files via upload

---
+++ Prolog/week3/assignment3.pl
@@ -0,0 +1,28 @@
+
+% A standard adder
+adder(Input1, Input2, Output):-
+	Output is Input1 + Input2.
+
+% A standard multiplier
+multiplier(Input1, Input2, Output):-
+	Output is Input1 * Input2.
+
+% Model 1 from the assignment
+model1(Answer) :-
+	adder(1, 1, T1),
+	multiplier(5, T1, T3),
+	multiplier(3, 2, T2),
+	adder(T2, T3, Answer).
+
+% Model 2 of the assignment
+model2(AnswerMultiplier, AnswerAdder) :-
+	multiplier(3, 2, T1),
+	multiplier(3, 2, T2),
+	multiplier(3, 2, T3),
+	adder(T1, T2, T4),
+	adder(T1, T3, T5),
+	adder(T2, T3, T6),
+	multiplier(T4, T5, T7),
+	multiplier(T5, T6, T8),
+	multiplier(T7, T8, AnswerMultiplier),
+	adder(T7, T8, AnswerAdder).
